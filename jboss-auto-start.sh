dae611707dcbf7432d9f23f75ec562c908822043
Robin <lhh_nj@163.com>
2020-05-18 22:41:02 +0800

    Create jboss-auto-start.sh

---
+++ jboss-auto-start.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+### BEGIN INIT INFO
+
+# Provides: jboss
+
+# Required-Start: $local_fs $remote_fs $network $syslog
+
+# Required-Stop: $local_fs $remote_fs $network $syslog
+
+# Default-Start: 2 3 4 5
+
+# Default-Stop: 0 1 6
+
+# Short-Description: Management of JBoss AS v7.x
+
+### END INIT INFO
+
+case "$1" in
+
+start)
+
+echo "Starting JBoss"
+
+sudo -u javau sh /apps/xxx/scripts/TPS/startJBoss --instanceName=xxxx
+
+;;
+
+stop)
+
+echo "Stopping JBoss"
+
+sudo -u javau sh /apps/xxx/scripts/TPS/stopJBoss --instanceName=xxxxx
+
+;;
+
+log)
+
+echo "Showing server.log..."
+
+tail -1000f /apps/xxx/logs/jboss-eap/700/TPS/xxxxx/server.log
+
+;;
+
+*)
+
+echo "Usage: /etc/init.d/jboss {start|stop|log}"
+
+exit 1
+
+;; esac
+
+exit 0
